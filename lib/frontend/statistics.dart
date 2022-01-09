da9b5f1eb2a69f02c3ac35e5422251d39cb38082
oksana <oksanadamarja@gmail.com>
2022-01-09 19:35:31 +0300

    statistics.dart is updated

--- lib/frontend/statistics.dart
+++ lib/frontend/statistics.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:workinn/TestConstants.dart';
 import 'package:workinn/repository/WorkoutRepository.dart';
+import 'widgets/StatisticsWidget.dart';
 
 class Statistics extends StatefulWidget {
   const Statistics({Key? key}) : super(key: key);
@@ -12,14 +13,45 @@ class Statistics extends StatefulWidget {
 class _StatisticsState extends State<Statistics> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FloatingActionButton(
-        backgroundColor: Colors.pink,
-        child: Icon(Icons.add),
-        onPressed: () {
-          
-        },
-      ),
-    );
+     return Container(
+      child: Scaffold(
+        body: ListView(children: [
+         StatisticsWidget.workoutStatisticsListviewItem(TestConstants.workout1, context),
+          ListTile(
+            title: Text("dlskfj"),
+          ),
+          ListTile(
+            title: Text("dlskfj"),
+          ),
+          ListTile(
+            title: Text("dlskfj"),
+          ),
+          ListTile(
+            title: Text("dlskfj"),
+          ),
+          ListTile(
+            title: Text("dlskfj"),
+          ),
+          ListTile(
+            title: Text("dlskfj"),
+          ),
+          ListTile(
+            title: Text("dlskfj"),
+          ),
+          ListTile(
+            title: Text("dlskfj"),
+          ),
+          ListTile(
+            title: Text("dlskfj"),
+          ),
+          ListTile(
+            title: Text("dlskfj"),
+          ),
+          ListTile(
+            title: Text("dlskfj"),
+          ),
+        ]),
+       
+      ),);
   }
 }
